527a1b812ce27d26ae908630d2f549be6d6f444d
Ibrahim Eshag <hemaisne@yahoo.com>
2025-02-02 23:13:21 +0300

    docs:  Solving Security issue

--- src/main/java/com/ibrahimeshag/basicspringsecutiywithjdbcuserdetailsmanager/config/ProjectConfig.java
+++ src/main/java/com/ibrahimeshag/basicspringsecutiywithjdbcuserdetailsmanager/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.ibrahimeshag.basicspringsecutiywithjdbcuserdetailsmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -15,7 +16,26 @@ import javax.sql.DataSource;
 public class ProjectConfig {
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
+        /*
+        JdbcUSerDetailsManager expects the database to have the following columns
+        User table:
+        username, password, and enabled.
+        authorities table:
+         username, authority
+
+        in that case returning the default instance of JdbcUserDetailsManager is enough, e.g:
         return new JdbcUserDetailsManager(dataSource);
+        */
+
+        // if the columns are different, then we need to add our custom implementation, as following
+        String usersByUsernameQuery =
+                "select username, password, enabled from users where username = ?";
+        String authsByUserQuery =
+                "select username, authority from authorities where username = ?";
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
     }
 
     @Bean
@@ -25,10 +45,18 @@ public class ProjectConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        /* `http.httpBasic(Customizer.withDefaults())` configures Spring Security to parse this header and authenticate the user
+          credentials against the configured authentication mechanism (in your case, `JdbcUserDetailsManager`).
+         Without this configuration, Spring Security doesn't process the `Authorization` header for Basic Authentication,
+         causing users to always be treated as unauthenticated (triggering the `AnonymousAuthenticationFilter`
+        */
+
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
+                                .requestMatchers("/h2-console/**").permitAll()
+//                        .requestMatchers("/hello").hasAuthority("write")
+                                .anyRequest().authenticated()
                 )
         ;
 
